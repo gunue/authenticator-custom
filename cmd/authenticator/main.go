167c0b1d65380a12548524a5bca404f582258b07
gunue <41422331+gunue@users.noreply.github.com>
2023-04-24 16:42:05 +0300

    Update main.go

--- cmd/authenticator/main.go
+++ cmd/authenticator/main.go
@@ -36,10 +36,10 @@ func main() {
 
 	// Configure exponential backoff
 	expBackoff := backoff.NewExponentialBackOff()
-	expBackoff.InitialInterval = 1 * time.Second
+	expBackoff.InitialInterval = 2 * time.Second
 	expBackoff.RandomizationFactor = 0.5
-	expBackoff.Multiplier = 1
-	expBackoff.MaxInterval = 1 * time.Second
+	expBackoff.Multiplier = 2
+	expBackoff.MaxInterval = 15 * time.Second
 	expBackoff.MaxElapsedTime = 2 * time.Minute
 
 	err = backoff.Retry(func() error {
@@ -70,5 +70,5 @@ func main() {
 
 func printErrorAndExit(errorMessage string) {
 	log.Error(errorMessage)
-	os.Exit(0)
+	os.Exit(1)
 }
