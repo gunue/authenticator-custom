2c0dc25aaf5ba3ace10484c6ea6a6e79f7e5e908
gunue <41422331+gunue@users.noreply.github.com>
2023-04-24 14:01:20 +0300

    maingo changed

--- cmd/authenticator/main.go
+++ cmd/authenticator/main.go
@@ -1,78 +1,74 @@
 package main
 
 import (
-        "context"
-        "fmt"
-        "os"
-        "time"
+	"context"
+	"fmt"
+	"os"
+	"time"
 
-        "github.com/cyberark/conjur-authn-k8s-client/pkg/authenticator"
-        "github.com/cyberark/conjur-authn-k8s-client/pkg/authenticator/config"
+	"github.com/cyberark/conjur-authn-k8s-client/pkg/authenticator"
+	"github.com/cyberark/conjur-authn-k8s-client/pkg/authenticator/config"
 
-        "github.com/cenkalti/backoff"
+	"github.com/cenkalti/backoff"
 
-        "github.com/cyberark/conjur-authn-k8s-client/pkg/log"
-        "github.com/cyberark/conjur-opentelemetry-tracer/pkg/trace"
+	"github.com/cyberark/conjur-authn-k8s-client/pkg/log"
+	"github.com/cyberark/conjur-opentelemetry-tracer/pkg/trace"
 )
 
 func main() {
-        log.Info(log.CAKC048, authenticator.FullVersionName)
+	log.Info(log.CAKC048, authenticator.FullVersionName)
 
-        var err error
+	var err error
 
-        config, err := config.NewConfigFromEnv()
-        if err != nil {
-                printErrorAndExit(log.CAKC018)
-        }
+	config, err := config.NewConfigFromEnv()
+	if err != nil {
+		printErrorAndExit(log.CAKC018)
+	}
 
-        tracer, _ := trace.NewTracerProvider(trace.NoopProviderType, false, trace.TracerProviderConfig{})
-        defer tracer.Shutdown(context.Background())
+	tracer, _ := trace.NewTracerProvider(trace.NoopProviderType, false, trace.TracerProviderConfig{})
+	defer tracer.Shutdown(context.Background())
 
-        // Create new Authenticator
-        authn, err := authenticator.NewAuthenticator(config)
-        if err != nil {
-                printErrorAndExit(log.CAKC019)
-        }
+	// Create new Authenticator
+	authn, err := authenticator.NewAuthenticator(config)
+	if err != nil {
+		printErrorAndExit(log.CAKC019)
+	}
 
-    // Configure exponential backoff
-    expBackoff := backoff.NewExponentialBackOff()
-    expBackoff.InitialInterval = 1 * time.Second
-    expBackoff.RandomizationFactor = 0
-    expBackoff.Multiplier = 1
-    expBackoff.MaxInterval = 1 * time.Second
-    expBackoff.MaxElapsedTime = 5 * time.Second
+	// Configure exponential backoff
+	expBackoff := backoff.NewExponentialBackOff()
+	expBackoff.InitialInterval = 1 * time.Second
+	expBackoff.RandomizationFactor = 0
+	expBackoff.Multiplier = 1
+	expBackoff.MaxInterval = 1 * time.Second
+	expBackoff.MaxElapsedTime = 2 * time.Minute
 
-    retryCount := 0
+	err = backoff.Retry(func() error {
+		for {
+			err := authn.AuthenticateWithContext(context.Background())
+			if err != nil {
+				return log.RecordedError(log.CAKC016)
+			}
 
-    _ = backoff.Retry(func() error {
-        retryCount++
-        err := authn.AuthenticateWithContext(context.Background())
-        if err != nil {
-            if retryCount >= 2 {
-                return nil
-            }
-            return log.RecordedError(log.CAKC016)
-        }
+			if config.GetContainerMode() == "init" {
+				os.Exit(0)
+			}
 
-        if config.GetContainerMode() == "init" {
-            os.Exit(0)
-        }
+			log.Info(log.CAKC047, config.GetTokenTimeout())
 
-        log.Info(log.CAKC047, config.GetTokenTimeout())
+			fmt.Println()
+			time.Sleep(config.GetTokenTimeout())
 
-        fmt.Println()
-        time.Sleep(config.GetTokenTimeout())
+			// Reset exponential backoff
+			expBackoff.Reset()
+		}
+	}, expBackoff)
 
-        // Reset exponential backoff
-        expBackoff.Reset()
-
-        return nil
-    }, expBackoff)
-
-    os.Exit(0)
+	if err != nil {
+		printErrorAndExit(log.CAKC031)
+	}
 }
 
 func printErrorAndExit(errorMessage string) {
-    log.Error(errorMessage)
-    os.Exit(1)
+	log.Error(errorMessage)
+	os.Exit(0)
 }
